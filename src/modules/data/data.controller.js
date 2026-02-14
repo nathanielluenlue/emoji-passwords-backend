615aa5b2c72ce07d7ef62f001b55037c1d1584cc
2778183L <278183L@student.gla.ac.uk>
2026-02-14 23:23:20 +0000

    Update data.controller.js

--- src/modules/data/data.controller.js
+++ src/modules/data/data.controller.js
@@ -49,7 +49,7 @@ async function updatePassword(req, res) {
 async function updateUsername(req, res) {
   try {
     console.log("update username");
-    const { username }= res
+    const { username } = req.body;
     const account = await service.updateUsername(username);
     res.json(account);
   } catch (err) {
