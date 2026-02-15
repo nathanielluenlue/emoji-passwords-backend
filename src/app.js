69ca0844c653c5fc5f5e2f7642c69837c6a29d74
2778183L <278183L@student.gla.ac.uk>
2026-02-15 00:47:10 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json({ limit: "1mb" }));
 app.use("/health", require("./modules/health/health.routes"));
 app.use("/data", require("./modules/data/data.routes"));
 
